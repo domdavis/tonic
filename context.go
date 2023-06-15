069330f447a512a5ce0beff2ce523f7d97c5c748
Dom Davis <dom@domdavis.com>
2023-06-15 10:28:51 +0100

    Fix #7: Allow type safe return of context values

--- context.go
+++ context.go
@@ -1,6 +1,7 @@
 package tonic
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,26 @@ func Get[T any](ctx *gin.Context, key string) T {
 	var t T
 
 	if v, ok := ctx.Get(key); !ok {
+		return t
+	} else if t, ok = v.(T); !ok {
+		panic(fmt.Sprintf("cannot get %T from context, value is %T", t, v))
+	}
+
+	return t
+}
+
+// Value returns the context value in a type safe manner. If no value is
+// associated with the key then the zero value for the type will be returned.
+// If the type cannot be cast to type T then Value will panic.
+func Value[T any](ctx context.Context, key string) T {
+	var (
+		ok bool
+		t  T
+	)
+
+	v := ctx.Value(key)
+
+	if v == nil {
 		return t
 	} else if t, ok = v.(T); !ok {
 		panic(fmt.Sprintf("cannot get %T from context, value is %T", t, v))
